1d0cf2d1ce9d73d6b6fd12d883612db3ba15d696
Software-Mods.com <scott.m.rowley@gmail.com>
2023-08-18 16:03:20 -0500

    Changed debug server to flask, changed FLASK_APP

--- debug_serve.sh
+++ debug_serve.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 source ../virtual_env/Scripts/activate
 
-export FLASK_APP=prod
+export FLASK_APP=app
 export FLASK_DEBUG=True
-waitress-serve --listen=*:8081 app:app
+flask run -h localhost -p 8081
 # in browser: http://localhost:8081
--- serve.sh
+++ serve.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 source ../virtual_env/Scripts/activate
 
-export FLASK_APP=prod
+export FLASK_APP=app
 export FLASK_DEBUG=False
 waitress-serve --listen=*:8081 app:app
 # in browser: http://localhost:8081
