5df07c6fc03d93f9105077efc27b5b7ed983a84b
Software-Mods.com <scott.m.rowley@gmail.com>
2023-08-18 19:56:26 -0500

    Added shopping list to homepage

--- routes/frontend.py
+++ routes/frontend.py
@@ -5,21 +5,33 @@
 # You can find out more about blueprints at
 # http://flask.pocoo.org/docs/blueprints/
 
-from flask import Blueprint, flash, make_response, redirect, request, session, url_for, render_template as real_render_template, Response # last one use for overriding and passing NAVIGATION to all templates
+from flask import (
+    Blueprint,
+    flash,
+    make_response,
+    redirect,
+    request,
+    session,
+    url_for,
+    render_template as real_render_template,
+    Response,
+)  # last one use for overriding and passing NAVIGATION to all templates
 from markupsafe import escape
 from helpers.common import render_template, loadJSONFromFile
 from helpers.tasks import task_file
 from helpers.bills import bill_file
 from helpers.chores import *
+from helpers.shopping import shopping_file
 from nav import NAVIGATION
 
-frontend = Blueprint('frontend', __name__)
+frontend = Blueprint("frontend", __name__)
+
 
 # Our index-page just shows a quick explanation. Check out the template
 # "templates/index.html" documentation for more details.
-@frontend.route('/')
+@frontend.route("/")
 def index():
-    """"default landing page"""
+    """ "default landing page"""
 
     tasks = loadJSONFromFile(task_file)
     bills = loadJSONFromFile(bill_file)
@@ -27,14 +39,27 @@ def index():
     floors = loadJSONFromFile(floors_file)
     locations = loadJSONFromFile(locations_file)
     assignees = loadJSONFromFile(assignee_file)
+    shopping_list = loadJSONFromFile(shopping_file)
 
-    return render_template('index.html', floors=floors, locations=locations, assignees=assignees, tables=True, chores=chores, tasks=tasks, bills=bills)
+    return render_template(
+        "index.html",
+        floors=floors,
+        locations=locations,
+        assignees=assignees,
+        tables=True,
+        chores=chores,
+        tasks=tasks,
+        bills=bills,
+        shopping=shopping_list
+    )
 
-@frontend.route('/icons')
+
+@frontend.route("/icons")
 def icons():
-
-    file1 = open('static/fontawesomefree/css/all.css', 'r')
+    file1 = open("static/fontawesomefree/css/all.css", "r")
     Lines = file1.readlines()
-    iconlist = [l.split('::')[0].split(':')[0].split('.')[1] for l in Lines if 'before' in l]
+    iconlist = [
+        l.split("::")[0].split(":")[0].split(".")[1] for l in Lines if "before" in l
+    ]
 
-    return render_template('icons.html', tables=True, icons=iconlist)
+    return render_template("icons.html", tables=True, icons=iconlist)
